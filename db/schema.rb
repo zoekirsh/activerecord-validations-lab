5e57031f9344d8b207bcc416242babad0460318f
Max Cantor <max@maxcantor.net>
2015-12-02 01:31:59 -0500

    First pass.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20151202061742) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string   "name"
-    t.string   "phone_number"
+    t.integer  "phone_number"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
