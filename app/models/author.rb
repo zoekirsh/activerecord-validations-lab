9d04c0990edd9ae536983c2cda4805b71a10aa5f
Zoe Kirsh <zoekirsh@gmail.com>
2020-12-17 18:33:26 -0800

    all passing! first and only win of the day *sob*

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :name
+  validates :name, uniqueness: true
+  validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :clickbait 
+
+  def clickbait
+    if self.title
+      if !["Won't Believe", "Secret", "Top 3", "Guess"].any? {|str| self.title.include? str}
+      errors.add(:title, "Title is not clickbait!")
+      end
+    end
+  end
 end
